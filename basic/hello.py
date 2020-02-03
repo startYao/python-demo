e70a9a7cb3bdecf0ffe9e806ea0d6d81974b34e6
yxy <429572661@qq.com>
2020-02-03 15:24:58 +0800

    初始化

---
+++ basic/hello.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# 可能出现乱码，那么就需要在头部添加 # -*- coding: UTF-8 -*- 或者 # coding=utf-8
+# 注意：# coding=utf-8 的 = 号两边不要空格。
+# Python3.X 源码文件默认使用utf-8编码，所以可以正常解析中文，无需指定 UTF-8 编码。
+
+print("你好,python")
+
